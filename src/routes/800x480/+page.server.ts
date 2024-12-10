b8eddd9fb48a130d9d5c3710f93896aeb294f4d5
ObsidianPresidium <emil@aarestrup.nu>
2024-12-10 21:49:14 +0100

    feat: remove console.logs and make desktop items visible

--- src/routes/800x480/+page.server.ts
+++ src/routes/800x480/+page.server.ts
@@ -1,5 +1,4 @@
 export function load({ cookies }) {
-    console.log(cookies.get("useCursors"));
     const useCursors = cookies.get("useCursors") === "true";
     return { useCursors: useCursors };
 }
--- src/routes/800x480/+page.svelte
+++ src/routes/800x480/+page.svelte
@@ -7,6 +7,7 @@
         align-items: center;
         width: 100%;
         height: 100%;
+        margin: 2rem 0;
     }
     .foreground {
         width: 100%;
@@ -17,7 +18,7 @@
     .panel {
         width: 100%;
         height: 10rem;
-        @include transparent-gradient-background(var(--gradient-8), 0.33);
+        @include transparent-gradient-background(var(--gradient-8), 0.33, $z-index: -1);
         display: flex;
         align-items: center;
     }
@@ -26,9 +27,13 @@
         margin: 0 2rem;
         display: block;
         @include transparent-gradient-background(var(--gradient-8), 0.33);
+        width: calc(50% - 4rem);
+        height: 100%;
     }
     .desktop-items {
         display: flex;
+        height: 100%;
+        width: 100%;
     }
 
     .foreground--no-cursor :global {
@@ -48,11 +53,7 @@
     import Button from "$lib/Button.svelte";
     
     let { data } = $props();
-    let foreground: HTMLDivElement;
-    
-    onMount(() => {
-        console.log(data);
-    });
+    let foreground: HTMLDivElement
 </script>
 
 <Background useCursors={data.useCursors} />
