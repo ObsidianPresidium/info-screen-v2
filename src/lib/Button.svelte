347f61d6559c7f20b66d88e678fa2f15de44880f
ObsidianPresidium <emil@aarestrup.nu>
2024-12-11 19:10:50 +0100

    feat: begin work on disabled buttons

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,29 +1,69 @@
 <style lang="scss">
     @import "@fontsource/cantarell/700";
 
-    .button-wrap {
-        perspective: 15rem;
-        perspective-origin: center;
-    }
-    .btn,
-    button {
-        --gradient-default: linear-gradient(135deg, rgba(46,161,255,1) 10%, rgb(116, 195, 255) 40%, rgba(0,161,255,1) 100%);
+    @mixin common-button-styles {
         color: white;
         padding: 1rem 2rem;
         font-family: "Cantarell", sans-serif;
         border-radius: 2rem;
         font-weight: bold;
         font-size: 3rem;
-        background-image: var(--gradient-default);
         background-size: 400% 100%;
         background-position-x: 0%;
+    }
+
+    .button-wrap {
+        perspective: 15rem;
+        perspective-origin: center;
+    }
+    .btn {
+        --gradient-default: linear-gradient(135deg, rgba(46,161,255,1) 10%, rgb(116, 195, 255) 40%, rgba(0,161,255,1) 100%);
+        --gradient-disabled: linear-gradient(135deg, rgba(98,98,98,1) 10%, rgba(150,150,150,1) 40%, rgba(23,23,23,1) 100%);
+        @include common-button-styles();
+        background-image: var(--gradient-default);
         transition: background-position-x 300ms cubic-bezier(0,0,0,1), transform 300ms ease-out;
+        position: relative;
 
         &:active {
             background-position-x: 100%;
         }
+
+        &--to-disabled {
+            cursor: not-allowed;
+            
+            &::after {
+                @include common-button-styles();
+                content: "";
+                background-image: var(--gradient-disabled);
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                animation: fade-in 300ms ease-out;
+                animation-iteration-count: 1;
+                animation-fill-mode: forwards;
+            }
+        }
+
+        &--to-enabled {
+            cursor: default;
+            &::after {
+                content: "";
+                @include common-button-styles();
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-image: var(--gradient-enabled);
+                animation: fade-in 300ms ease-out;
+                animation-iteration-count: 1;
+                animation-fill-mode: forwards;
+            }
+        }
     }
-    .btn {
+    a.btn {
         &,
         &:link,
         &:visited {
@@ -33,9 +73,17 @@
             border-radius: 2rem;
         }
     }
-
-    button {
+    button.btn {
         border: none;
+    }
+
+    @keyframes fade-in {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
     }
 </style>
 
@@ -43,8 +91,11 @@
     import { onMount } from 'svelte';
     let { text, href = "#", gradient = true, isButtonElement = true, zIndex = "auto", usePunchyClick = true } = $props();
 
+    const perspectiveFactor = 0.75;  // Increase this for a smaller FoV
+
     let callback: string | ((event: MouseEvent) => void) = href;
     let elButton: HTMLAnchorElement | HTMLButtonElement;
+    let elDiv: HTMLDivElement;
     
     let punchyClick: EventListener = (event) => {
         if (!usePunchyClick) {
@@ -97,11 +148,12 @@
                 elButton.type = "submit";
             }
         }
+        elDiv.style.perspective = `${Number(getComputedStyle(elButton).width.replace("px", "")) * perspectiveFactor}px`;
     });
 </script>
 
 <!-- svelte-ignore a11y_missing_attribute, a11y_no_static_element_interactions -->
-<div class="button-wrap" onmousedown={punchyClick} onmouseup={unpunch} onmouseleave={unpunch}>
+<div class="button-wrap" bind:this={elDiv} onmousedown={punchyClick} onmouseup={unpunch} onmouseleave={unpunch}>
     {#if isButtonElement}
         <button bind:this={elButton}>{text}</button>
     {:else}
