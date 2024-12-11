dfc213a459ac341e0297e237d0dbc54374056752
ObsidianPresidium <emil@aarestrup.nu>
2024-12-11 19:10:27 +0100

    feat: move slider into its own component

---
+++ src/lib/Slider.svelte
@@ -0,0 +1,74 @@
+<style lang="scss">
+    .slider-div {
+        display: flex;
+        align-items: center;
+        p {
+            color: white;
+            font-size: 3rem;
+        }
+    }
+    .switch {
+        position: relative;
+        display: inline-block;
+        cursor: pointer;
+        width: 6rem;
+        height: 3rem;
+        margin-right: 1rem;
+
+        input {
+            opacity: 0;
+            width: 0;
+            height: 0;
+            
+            &:checked + .slider {
+                background-color: rgb(80, 156, 255);
+            }
+
+            &:checked + .slider:before {
+                transform: translateX(2.75rem);
+                background-color: white;
+            }
+        }
+        
+        .slider {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: white;
+            border-radius: 3rem;
+            transition: background-color 400ms;
+
+            &:before {
+                content: "";
+                position: absolute;
+                width: 2.5rem;
+                height: 2.5rem;
+                left: 0.25rem;
+                bottom: 0.25rem;
+                background-color: #222;
+                transition: 400ms;
+                border-radius: 50%;
+            }
+        }
+    }
+</style>
+
+<script lang="ts">
+    import { onMount } from "svelte";
+    let { text, defaultChecked = false, checked = $bindable() } = $props();
+
+    onMount(() => {
+        checked = defaultChecked;
+    });
+
+</script>
+
+<div class="slider-div">
+    <label for="use-cursors" class="switch">
+        <input type="checkbox" name="use-cursors" id="use-cursors" bind:checked={checked}>
+        <span class="slider"></span>
+    </label>
+    <p>{text}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,66 +25,11 @@
             margin-left: 1rem;
         }
     }
-    .slider-div {
-        display: flex;
-        align-items: center;
-        p {
-            color: white;
-            font-size: 3rem;
-        }
-    }
-    .switch {
-        position: relative;
-        display: inline-block;
-        cursor: pointer;
-        width: 6rem;
-        height: 3rem;
-        margin-right: 1rem;
-
-        input {
-            opacity: 0;
-            width: 0;
-            height: 0;
-            
-            &:checked + .slider {
-                background-color: rgb(80, 156, 255);
-            }
-
-            &:checked + .slider:before {
-                transform: translateX(2.75rem);
-                background-color: white;
-            }
-        }
-        
-        .slider {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: white;
-            border-radius: 3rem;
-            transition: background-color 400ms;
-
-            &:before {
-                content: "";
-                position: absolute;
-                width: 2.5rem;
-                height: 2.5rem;
-                left: 0.25rem;
-                bottom: 0.25rem;
-                background-color: #222;
-                transition: 400ms;
-                border-radius: 50%;
-            }
-        }
-
-
-    }
 </style>
 
 <script lang="ts">
     import Button from "$lib/Button.svelte";
+    import Slider from "$lib/Slider.svelte";
     import { onMount } from "svelte";
     let { userLikesCursorsBool } = $props();
     let useCursorsCheckboxChecked = $state(userLikesCursorsBool);
@@ -109,13 +54,7 @@
                 <input type="radio" name="size" id="size-full" value="full"><label for="size-full">Full size (scale with browser viewport)</label>
             </div>
         </div>
-        <div class="slider-div">
-            <label for="use-cursors" class="switch">
-                <input type="checkbox" name="use-cursors" id="use-cursors" bind:checked={useCursorsCheckboxChecked}>
-                <span class="slider"></span>
-            </label>
-            <p>Display cursor (enable this if not using touchscreen)</p>
-        </div>
-        <Button text="Start info-screen-v2" isButtonElement/>
+        <Slider text="Display cursor (enable this if not using touchscreen)" bind:checked={useCursorsCheckboxChecked} defaultChecked={userLikesCursorsBool} />
+        <Button text="Start info-screen-v2" isButtonElement />
     </form>
 </main>
